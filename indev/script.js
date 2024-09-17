b0b62826a66eff1bbb028ae00b61e5b4a47e5200
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-09-17 13:35:07 -0500

    kjsxc njsjdiocnaosd variables

--- indev/script.js
+++ indev/script.js
@@ -2,7 +2,14 @@ import { beginnerMath } from "./beginner/beginner.js";
 
 var mathMod = new beginnerMath();
 
-document.getElementById('1p1').innerHTML = mathMod.add(1, 1).toString();
-document.getElementById('10t50').innerHTML = mathMod.multiply(10, 50).toString();
-document.getElementById('60s20').innerHTML = mathMod.subtract(60, 20).toString();
-document.getElementById('100d50').innerHTML = mathMod.divide(100, 50).toString();
+var q1 = document.getElementById('1p1');
+q1.innerHTML = mathMod.add(1, 1).toString();
+
+var q2 = document.getElementById('10t50');
+q2.innerHTML = mathMod.multiply(10, 50).toString();
+
+var q3 = document.getElementById('60s20');
+q3.innerHTML = mathMod.subtract(60, 20).toString();
+
+var q4 = document.getElementById('100d50');
+q4.innerHTML = mathMod.divide(100, 50).toString();
