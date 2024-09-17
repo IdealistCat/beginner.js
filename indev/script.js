d2d62427307b19929352eb08b377b8e01fb755b9
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-09-17 13:32:43 -0500

    dont forget

--- indev/script.js
+++ indev/script.js
@@ -2,7 +2,7 @@ import { beginnerMath } from "./beginner/beginner.js";
 
 var mathMod = new beginnerMath();
 
-document.getElementById('1p1').innerHTML = mathMod.add(1, 1);
-document.getElementById('10t50').innerHTML = mathMod.multiply(10, 50);
-document.getElementById('60s20').innerHTML = mathMod.subtract(60, 20);
-document.getElementById('100d50').innerHTML = mathMod.divide(100, 50);
+document.getElementById('1p1').innerHTML = mathMod.add(1, 1).toString();
+document.getElementById('10t50').innerHTML = mathMod.multiply(10, 50).toString();
+document.getElementById('60s20').innerHTML = mathMod.subtract(60, 20).toString();
+document.getElementById('100d50').innerHTML = mathMod.divide(100, 50).toString();
