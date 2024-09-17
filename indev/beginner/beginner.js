75987314239e1ef99cb8b62209f30cd62256a857
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-09-17 12:34:41 -0500

    the first class

---
+++ indev/beginner/beginner.js
@@ -0,0 +1,6 @@
+export class Math {
+    add(num1 = 0, num2 = 0) { return num1 + num2; }
+    subtract(num1 = 0, num2 = 0) { return num1 - num2; }
+    multiply(num1 = 0, num2 = 0) { return num1 * num2; }
+    divide(num1 = 0, num2 = 0) { return num1 / num2; }
+}
